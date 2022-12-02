28c45c4e00205eb9cc582a12e8c7ba94454181f4
Daniel Lawhorn <dalawhorn21@gmail.com>
2022-12-02 09:12:56 -0500

    Day 2 init.

---
+++ day2.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const inputFile = "./day2input.txt";
+
+fileStream.on('data', function(chunk) {
+
+});
+
+fileStream.on('error', function(error) {
+  console.log(error);
+});
