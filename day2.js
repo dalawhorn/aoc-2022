47c7b6b81372bc841e1770b7916dd40d5d5426ed
Daniel Lawhorn <dalawhorn21@gmail.com>
2022-12-02 13:37:46 -0500

    Day 2 Part 2.

--- day2.js
+++ day2.js
@@ -13,6 +13,11 @@ const col1Scores = {
       X: "draw",
       Y: "lose",
       Z: "win"
+    },
+    col2Outcomes: {
+      win: "Y",
+      lose: "Z",
+      draw: "X"
     }
   },
   B: {
@@ -22,6 +27,11 @@ const col1Scores = {
       X: "win",
       Y: "draw",
       Z: "lose"
+    },
+    col2Outcomes: {
+      win: "Z",
+      lose: "X",
+      draw: "Y"
     }
   },
   C: {
@@ -31,6 +41,11 @@ const col1Scores = {
       X: "lose",
       Y: "win",
       Z: "draw"
+    },
+    col2Outcomes: {
+      win: "X",
+      lose: "Y",
+      draw: "Z"
     }
   }
 };
@@ -71,8 +86,16 @@ const outcomeScores = {
   draw: 3
 }
 
+const part2Outcomes = {
+  X: "lose",
+  Y: "draw",
+  Z: "win"
+}
+
 let col1Score = 0;
 let col2Score = 0;
+
+let col2ScorePart2 = 0;
 
 fileStream.on('data', function(chunk) {
   const fileData = chunk.toString();
@@ -85,12 +108,17 @@ fileStream.on('data', function(chunk) {
     
     col1Score = col1Score + col1ScoreCalc;
     col2Score = col2Score + col2ScoreCalc;
+
+    let part2Score = calculateScorePart2(col1Scores[values[0]], values[1]);
+    col2ScorePart2 = col2ScorePart2 + part2Score;
   });
 });
 
 fileStream.on('end', function() {
   console.log("Col 1", "Col 2");
   console.log(col1Score, col2Score);
+  console.log("Part 2");
+  console.log(col2ScorePart2);
 });
 
 fileStream.on('error', function(error) {
@@ -101,4 +129,11 @@ function calculateScore(thisColumnData, versusColumnValue) {
   const outcome = thisColumnData.outcomes[versusColumnValue];
   const outcomeScore = outcomeScores[outcome];
   return thisColumnData.score + outcomeScore;
+}
+
+function calculateScorePart2(col1Data, col2Outcome) {
+  const outcomeType = part2Outcomes[col2Outcome];
+  const outcomeMove = col1Data.col2Outcomes[outcomeType];
+  const col2Score = col2Scores[outcomeMove].score;
+  return col2Score + outcomeScores[outcomeType];
 }
