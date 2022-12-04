92202c33031f866acadf3e50e800bcc90648fb07
Daniel Lawhorn <dalawhorn21@gmail.com>
2022-12-04 16:42:19 -0500

    Day 4 part 2.

--- day4/day4.js
+++ day4/day4.js
@@ -6,6 +6,7 @@ const inputFile = "./day4/day4input.txt";
 let fileStream = fs.createReadStream(inputFile);
 
 let pairsThatContain = [];
+let pairsThatOverlap = [];
 
 fileStream.on('data', function(chunk) {
   const fileData = chunk.toString();
@@ -16,8 +17,17 @@ fileStream.on('data', function(chunk) {
     const pair1 = sectionPairs[0].split('-');
     const pair2 = sectionPairs[1].split('-');
 
+    //part1
     if(rangePairContains(pair1, pair2)) {
         pairsThatContain.push(sectionPairs);
+        //part2 - if the pair contains then it's overlapped, skip the next check.
+        pairsThatOverlap.push(sectionPairs);
+        return;
+    }
+
+    //part2 - rest of ranges go through here..
+    if(rangePairOverlaps(pair1, pair2)) {
+        pairsThatOverlap.push(sectionPairs);
     }
   });
 });
@@ -25,6 +35,7 @@ fileStream.on('data', function(chunk) {
 fileStream.on('end', function() {
   //Do stuff after file processed...
   console.log("Total pairs that fully contain each other: ",pairsThatContain.length);
+  console.log("Total pairs that overlap: ",pairsThatOverlap.length);
 });
 
 fileStream.on('error', function(error) {
@@ -42,6 +53,23 @@ function rangePairContains(pair1, pair2) {
         if(parseInt(pair1[1]) >= parseInt(pair2[1])) {
             return true;
         }
+    }
+
+    return false;
+}
+
+function rangePairOverlaps(pair1, pair2) {
+    pair1 = [parseInt(pair1[0]), parseInt(pair1[1])];
+    pair2 = [parseInt(pair2[0]), parseInt(pair2[1])];
+
+    const all = pair1.concat(pair2);
+    const allSort = all.sort();
+
+    if( (pair2[0] >= pair1[0] && pair2[0] <= pair1[1]) 
+        || (pair2[1] >= pair1[0] && pair2[1] <= pair1[1]) 
+        || (pair1[0] >= pair2[0] && pair1[0] <= pair2[1]) 
+        || (pair1[1] >= pair2[0] && pair1[1] <= pair2[1]) ){
+        return true;
     }
 
     return false;
