07a064dc22c4ec3d5fc7f44e42587045a44a1730
Daniel Lawhorn <dalawhorn21@gmail.com>
2022-12-04 15:06:39 -0500

    Day 4 part 1.

--- day4/day4.js
+++ day4/day4.js
@@ -2,23 +2,47 @@
 
 const fs = require('fs');
 
-const inputFile = "./day4/day4inputtest.txt";
+const inputFile = "./day4/day4input.txt";
 let fileStream = fs.createReadStream(inputFile);
+
+let pairsThatContain = [];
 
 fileStream.on('data', function(chunk) {
   const fileData = chunk.toString();
   const dataArr = fileData.split('\n');
 
   dataArr.forEach(function(row, index) {
-    //Do stuff with file data...
-    console.log(row);
+    const sectionPairs = row.split(',');
+    const pair1 = sectionPairs[0].split('-');
+    const pair2 = sectionPairs[1].split('-');
+
+    if(rangePairContains(pair1, pair2)) {
+        pairsThatContain.push(sectionPairs);
+    }
   });
 });
 
 fileStream.on('end', function() {
   //Do stuff after file processed...
+  console.log("Total pairs that fully contain each other: ",pairsThatContain.length);
 });
 
 fileStream.on('error', function(error) {
   console.log(error);
 });
+
+function rangePairContains(pair1, pair2) {
+    if(parseInt(pair1[0]) >= parseInt(pair2[0])) {
+        if(parseInt(pair1[1]) <= parseInt(pair2[1])) {
+            return true;
+        }
+    }
+
+    if(parseInt(pair1[0]) <= parseInt(pair2[0])) {
+        if(parseInt(pair1[1]) >= parseInt(pair2[1])) {
+            return true;
+        }
+    }
+
+    return false;
+}
