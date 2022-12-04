f3d6c6f08e57f9e49c8a865db8ed8d7fe05f69a9
Daniel Lawhorn <dalawhorn21@gmail.com>
2022-12-03 20:33:37 -0500

    Day 3 part 2.

--- day3.js
+++ day3.js
@@ -8,12 +8,16 @@ let fileStream = fs.createReadStream(inputFile);
 const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const alphaArr = alpha.split('');
 let prioritySum = 0;
+let prioritySumPart2 = 0;
 
 fileStream.on('data', function(chunk) {
   const fileData = chunk.toString();
   const dataArr = fileData.split('\n');
 
+  let workingRows = [];
+
   dataArr.forEach(function(row, index) {
+    //Part 1 Code
     const totalItems = row.length;
     const compartmentItemsTotal = totalItems/2;
   
@@ -27,11 +31,23 @@ fileStream.on('data', function(chunk) {
     })
 
     prioritySum += (alphaArr.findIndex(letter => letter == compartmentIntersection[0]) + 1);
+
+    //Part 2 Code
+    workingRows.push(row.split(''));
+    if(((index+1) % 3) === 0) {
+      //Do comparison after getting 3 rows.
+      //NGL I gave up and copy/pasted this line from SO to compare 3 arrays at the same time, need to go back and learn how it really works...
+      const commonVal = workingRows.reduce((a, b) => a.filter(c => b.includes(c)));
+
+      prioritySumPart2 += (alphaArr.findIndex(letter => letter == commonVal[0]) + 1);;
+      workingRows = [];
+    }
   });
 });
 
 fileStream.on('end', function() {
-  console.log("Priority Sum: ", prioritySum);
+  console.log("Priority Sum Part 1: ", prioritySum);
+  console.log("Priority Sum Part 2: ", prioritySumPart2);
 });
 
 fileStream.on('error', function(error) {
