e5f3475104503dd97132e7bffc618b0b3988c564
Daniel Lawhorn <dalawhorn21@gmail.com>
2022-12-06 10:38:29 -0500

    Day 6 part 1

--- day6/day6.js
+++ day6/day6.js
@@ -10,7 +10,37 @@ fileStream.on('data', function(chunk) {
   const dataArr = fileData.split('\n');
 
   dataArr.forEach(function(row, index) {
-    //Do stuff with each row of file data here...
+    const characters = row.split('');
+    
+    let currentFour = [];
+    let lastCharIndex = 0;
+    let isUnique = false;
+    for(let i=0; i < characters.length; i++) {
+      if(currentFour.length < 4) {
+        currentFour.push(characters[i]);
+        lastCharIndex = i;
+      }
+      
+      if(currentFour.length === 4) {
+        // Check for duplicate chars here...
+        const unique = [...new Set(currentFour)];
+        if(unique.length === 4) {
+          // All characters are unique. We have a winner!
+          isUnique = true;
+          break;
+        }
+
+        // If there are duplicate chars, remove first char on current arr so next char can be added.
+        currentFour.shift();
+      }
+    }
+
+    if(isUnique){
+      console.log("Last Char With Unique Prev 4: ", lastCharIndex+1);
+    }
+    else {
+      console.log("No unique sequence was found");
+    }
   });
 });
 
