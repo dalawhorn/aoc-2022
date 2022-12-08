e4b2c71caa518c264724574ffa186d10252b87f8
Daniel Lawhorn <dalawhorn21@gmail.com>
2022-12-08 09:04:06 -0500

    Day 8 init

---
+++ day8/day8.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const inputFile = "./day8/day8input.txt";
+let fileStream = fs.createReadStream(inputFile);
+
+fileStream.on('data', function(chunk) {
+  const fileData = chunk.toString();
+  const dataArr = fileData.split('\n');
+
+  dataArr.forEach(function(row, index) {
+    //Do stuff with each row of file data here...
+  });
+});
+
+fileStream.on('end', function() {
+  //Do stuff after file processed here...
+});
+
+fileStream.on('error', function(error) {
+  console.log(error);
+});
