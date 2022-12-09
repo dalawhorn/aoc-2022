bf0e8ac106cb08f8956ee72cd940f66897f6d06a
Daniel Lawhorn <dalawhorn21@gmail.com>
2022-12-08 19:31:40 -0500

    Day 8 part 2.

--- day8/day8.js
+++ day8/day8.js
@@ -9,6 +9,7 @@ let totalColumns = 0;
 let totalRows = 0;
 let totalPerimeter = 0;
 let totalVisible = 0;
+let highestTreeScore = 0;
 let treeGrid = [];
 
 fileStream.on('data', function(chunk) {
@@ -27,72 +28,59 @@ fileStream.on('end', function() {
   totalVisible += totalPerimeter
 
   for(let row=1; row<(totalRows-1); row++) {
-    // searchPositionIndex['row'] = row;
-    // searchPositionIndex['col'] = 1;
-
     for(let col=1; col<(totalColumns-1); col++) {
+      let surroundingTrees = getSurroundingTrees(row, col);
+
+      // Part1
       // console.log('row', row, 'col', col);
       let currentTreeHeight = treeGrid[row][col];
       // console.log('current tree height', currentTreeHeight);
-      let isVisible = checkIfTreeVisible(row, col, currentTreeHeight);
+      let isVisible = checkIfTreeVisible(surroundingTrees, currentTreeHeight);
       if(isVisible) {
         // console.log('Visible!');
         totalVisible++;
+      }
+
+      // Part 2
+      let treeScore = calculateTreeViewScore(surroundingTrees, currentTreeHeight);
+      if(treeScore > highestTreeScore) {
+        highestTreeScore = treeScore;
       }
     }
   }
 
   console.log('Rows', totalRows, 'Cols', totalColumns, 'Perimeter', totalPerimeter);
   console.log('Visible', totalVisible);
+  console.log('Highest Tree Score', highestTreeScore);
 });
 
 fileStream.on('error', function(error) {
   console.log(error);
 });
 
-function checkIfTreeVisible(row, col, height) {
-  let left = [];
-  treeGrid[row].forEach(function(heightVal, index) {
-    if(index < col) {
-      left.push(heightVal);
-    }
-  });
+function checkIfTreeVisible(surroundingTrees, height) {
+  let left = surroundingTrees['left'];
   // console.log('left', left);
   if(compareHeightVal(left, height)) {
     console.log('Tree is visible to left');
     return true;
   }
 
-  let right = []
-  treeGrid[row].forEach(function(heightVal, index) {
-    if(index > col) {
-      right.push(heightVal);
-    }
-  });
+  let right = surroundingTrees['right'];
   // console.log('right', right);
   if(compareHeightVal(right, height)) {
     console.log('Tree is visible to right');
     return true;
   }
 
-  let up = []
-  treeGrid.forEach(function(treeRow, rowIndex) {
-    if(rowIndex != row && rowIndex < row){
-      up.push(treeRow[col]);
-    }
-  });
+  let up = surroundingTrees['up'];
   // console.log('up', up);
   if(compareHeightVal(up, height)) {
     console.log('Tree is visible up');
     return true;
   }
 
-  let down = []
-  treeGrid.forEach(function(treeRow, rowIndex) {
-    if(rowIndex != row && rowIndex > row){
-      down.push(treeRow[col]);
-    }
-  });
+  let down = surroundingTrees['down'];
   // console.log('down', down);
   if(compareHeightVal(down, height)) {
     console.log('Tree is visible down');
@@ -100,6 +88,52 @@ function checkIfTreeVisible(row, col, height) {
   }
 
   return false;
+}
+
+function getSurroundingTrees(row, col) {
+  return trees = {
+    left: getTreesByDirection('left', row, col),
+    right: getTreesByDirection('right', row, col),
+    up: getTreesByDirection('up', row, col),
+    down: getTreesByDirection('down', row, col)
+  }
+}
+
+function getTreesByDirection(direction, row, col) {
+  let returnTrees = [];
+
+  switch(direction) {
+    case "left":
+      treeGrid[row].forEach(function(heightVal, index) {
+        if(index < col) {
+          returnTrees.push(heightVal);
+        }
+      });
+      break;
+    case "right":
+      treeGrid[row].forEach(function(heightVal, index) {
+        if(index > col) {
+          returnTrees.push(heightVal);
+        }
+      });
+      break;
+    case "up":
+      treeGrid.forEach(function(treeRow, rowIndex) {
+        if(rowIndex != row && rowIndex < row){
+          returnTrees.push(treeRow[col]);
+        }
+      });
+      break;
+    case "down":
+      treeGrid.forEach(function(treeRow, rowIndex) {
+        if(rowIndex != row && rowIndex > row){
+          returnTrees.push(treeRow[col]);
+        }
+      });
+      break;
+  }
+
+  return returnTrees;
 }
 
 function compareHeightVal(arrVals, heightVal) {
@@ -116,4 +150,53 @@ function compareHeightVal(arrVals, heightVal) {
   }
 
   return false;
+}
+
+function calculateTreeViewScore(surroundingTrees, height) {
+  let leftTotal = getDirectionTreeViewTotal(surroundingTrees['left'], height, 'left');
+  let rightTotal = getDirectionTreeViewTotal(surroundingTrees['right'], height, 'right');
+  let upTotal = getDirectionTreeViewTotal(surroundingTrees['up'], height, 'up');
+  let downTotal = getDirectionTreeViewTotal(surroundingTrees['down'], height, 'down');
+
+  // console.log(surroundingTrees, height);
+  // console.log(upTotal, leftTotal, downTotal, rightTotal);
+  return (upTotal * leftTotal * downTotal * rightTotal);
+}
+
+function getDirectionTreeViewTotal(directionTrees, height, direction) {
+  const numTrees = directionTrees.length;
+  let treeTotal = 0;
+
+  switch(direction) {
+    case "left":
+    case "up":
+      for(i=(numTrees-1); i>=0; i--) {
+        let tree = directionTrees[i];
+        treeTotal++;
+    
+        if(tree >= height) {
+          //Found a tree that is the same or taller than the current tree height
+          //so we stop
+          break;
+        }
+      }
+      break;
+    case "right":
+    case "down":
+      for(i=0; i<numTrees; i++) {
+        let tree = directionTrees[i];
+        treeTotal++;
+    
+        if(tree >= height) {
+          //Found a tree that is the same or taller than the current tree height
+          //so we stop
+          break;
+        }
+      }
+      break;
+  }
+
+  
+
+  return treeTotal;
 }
